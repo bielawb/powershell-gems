3744d9c81c14582059df9ceb6cde33bc4dd014da
bielawb <bartek.bielawski@live.com>
2026-05-19 17:41:17 +0200

    Adding slides/ pictures

---
+++ Presentation/0.ps1
@@ -0,0 +1,22 @@
+﻿$3 = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '3.xaml' })[0]
+$2 = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '2.xaml' })[0]
+$1 = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '1.xaml' })[0]
+$0 = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '0.ps1' })[0]
+$main = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '00_Title.xaml' })[0]
+
+$demoGodsFailedMe = $true
+Start-Sleep -Milliseconds 100
+$wshell.SendKeys('^r')
+do {
+    if ($justTesting) {
+        break
+    }
+    Start-Sleep -Seconds 10
+} while ((Get-Date).Hour -lt 13)
+
+# 3... 2... 1... countdown - PS ISE style. :P
+foreach ($file in $0, $3, $2, $1) {
+    Start-Sleep -Seconds 1
+    $null = $psISE.CurrentPowerShellTab.Files.Remove($file)
+}
+$psISE.CurrentPowerShellTab.Files.SetSelectedFile($main)
