be0585b5f0f68270f99fb8893020683f0be24ed1
bielawb <bartek.bielawski@live.com>
2026-06-01 16:02:30 +0200

    Add 2 more demo scripts

--- Demos/03_VarIsFile.ps1
+++ Demos/03_VarIsFile.ps1
@@ -2,3 +2,16 @@
     Write-Warning -Message "Stop pressing F5!"
     break
 }
+
+${C:\Windows\system32\drivers\etc\hosts}
+
+${function:Get-Foo} = {
+    param (
+        [String]$Bar
+    )
+
+    "Bar: $Bar"
+}
+
+Get-Foo -Bar IsBar
+
--- Demos/04_Globbing.ps1
+++ Demos/04_Globbing.ps1
@@ -2,3 +2,6 @@
     Write-Warning -Message "Stop pressing F5!"
     break
 }
+
+ls C:\wi*\*\dr*\*c\h*
+ls C:\host[s] -Recurse -EA Si
